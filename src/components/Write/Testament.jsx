8a24b5948fb445acf62a7b042535de8fc979aaad
minibxx <alsdbqls1004@gmail.com>
2024-03-15 16:47:32 +0900

    feat: 유서 작성 페이지 구현중

--- src/components/Write/Testament.jsx
+++ src/components/Write/Testament.jsx
@@ -9,7 +9,6 @@ function Testament() {
       <Typography title={'나의 유서 작성하기'} type={'bold30'}/>
       <Typography title={'당신의 유서를 아래의 양식에 맞게 작성해 주세요.'} type={'regular15'}/>
       <Typography title={'법적으로 유서에는 작성자의 이름과 주소, 날인이 들어가야 합니다.'} type={'regular15'}/>
-      <Typography title={'당신의 유서를 아래의 양식에 맞게 작성해 주세요.'} type={'regular15'}/>
 
       <div className='flex flex-row mt-[30px] mb-[10px]'>
         <div className='mr-[20px]'>이름</div>
