f63a6f0593099a56f2ddb14ff476f8851e55aa48
sem-git <lsmsm0327@naver.com>
2024-03-16 22:35:05 +0900

    feat: HomePage 상단바 적용

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,24 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import TopBar from '@components/TopBar';
+
+const Screen = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <Screen>
+      <TopBar />
+    </Screen>
+  );
 };
 
 export default HomePage;
