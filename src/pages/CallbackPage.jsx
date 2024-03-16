7234bd3b1a795e7175f7d2580faa232de3248a79
sem-git <lsmsm0327@naver.com>
2024-03-16 22:43:41 +0900

    chore: react-spinners 추가

--- src/pages/CallbackPage.jsx
+++ src/pages/CallbackPage.jsx
@@ -1,5 +1,10 @@
-const CallbackPage = () => {
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import FadeLoader from 'react-spinners/FadeLoader';
+
+const CallBackPage = () => {
   return <>CallbackPage</>;
 };
 
-export default CallbackPage;
+export default CallBackPage;
