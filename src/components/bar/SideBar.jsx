708b1337686ec20552638f58335088e6bcd35418
sem-git <lsmsm0327@naver.com>
2024-03-19 04:10:10 +0900

    feat: SideBar에 유서 쓰기 추가

--- src/components/bar/SideBar.jsx
+++ src/components/bar/SideBar.jsx
@@ -30,6 +30,7 @@ const Container = styled.div`
 `;
 
 const IconContainer = styled.div`
+  padding-top: 3rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -156,6 +157,41 @@ const SideBar = () => {
 
       <NavLink
         to={`/write`}
+        className={({ isActive }) =>
+          isActive || location.pathname === `/` ? 'active' : 'inactive'
+        }
+      >
+        <IconContainer>
+          <span>유서 쓰기</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="11"
+            height="16"
+            viewBox="0 0 11 16"
+            fill="none"
+          >
+            <g clip-path="url(#clip0_523_6026)">
+              <path
+                d="M0 1.19175L1.04607 0L11 8L9.73627 9.0175L1.04607 16L0 14.8082L8.47253 8L0 1.19175Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_523_6026">
+                <rect
+                  width="11"
+                  height="16"
+                  fill="white"
+                  transform="matrix(-1 0 0 1 11 0)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </IconContainer>
+      </NavLink>
+
+      <NavLink
+        to={`/`}
         className={({ isActive }) =>
           isActive || location.pathname === `/` ? 'active' : 'inactive'
         }
