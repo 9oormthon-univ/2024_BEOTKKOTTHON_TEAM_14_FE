193896cd6654e869feeda9c5002b487ff5058eef
sem-git <lsmsm0327@naver.com>
2024-03-23 01:21:26 +0900

    feat: HomePage 메뉴 버튼 추가

--- src/pages/service/HomePage.jsx
+++ src/pages/service/HomePage.jsx
@@ -8,6 +8,8 @@ import home_1 from '@assets/home_1.png';
 import home_2 from '@assets/home_2.png';
 import home_3 from '@assets/home_3.png';
 import home_4 from '@assets/home_4.png';
+import home_6 from '@assets/home_6.png';
+import home_7 from '@assets/home_7.png';
 
 const Screen = styled.div`
   position: relative;
@@ -136,39 +138,13 @@ const HomePage = () => {
     <Screen>
       <Logo>
         <img src={home_logo} />
-
         <br />
+
         <span>
           당신이 내일 죽는다면? <br />
           플랜데스와 함께 <br />
           삶과 죽음을 기획해보세요.
         </span>
-
-        <br />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="52"
-          height="27"
-          viewBox="0 0 52 27"
-          fill="none"
-        >
-          <path
-            d="M26.7372 16.8651L50.1199 3.36511C50.8373 2.9509 51.081 2.02971 50.664 1.30757C50.2471 0.585439 49.3275 0.33582 48.6101 0.750033L25.2274 14.25C24.51 14.6642 24.2664 15.5854 24.6833 16.3076C25.1002 17.0297 26.0198 17.2793 26.7372 16.8651Z"
-            fill="white"
-          />
-          <path
-            d="M1.29875 3.49805L24.6814 16.998C25.3989 17.4123 26.3185 17.1626 26.7354 16.4405C27.1523 15.7184 26.9087 14.7972 26.1912 14.383L2.80856 0.882968C2.09112 0.468755 1.17154 0.718373 0.754615 1.44051C0.337691 2.16264 0.581307 3.08383 1.29875 3.49805Z"
-            fill="white"
-          />
-          <path
-            d="M26.7372 25.8651L50.1199 12.3651C50.8373 11.9509 51.081 11.0297 50.664 10.3076C50.2471 9.58544 49.3275 9.33582 48.6101 9.75003L25.2274 23.25C24.51 23.6642 24.2664 24.5854 24.6833 25.3076C25.1002 26.0297 26.0198 26.2793 26.7372 25.8651Z"
-            fill="white"
-          />
-          <path
-            d="M1.29875 12.498L24.6814 25.998C25.3989 26.4123 26.3185 26.1626 26.7354 25.4405C27.1523 24.7184 26.9087 23.7972 26.1912 23.383L2.80856 9.88297C2.09112 9.46875 1.17154 9.71837 0.754615 10.4405C0.337691 11.1626 0.581307 12.0838 1.29875 12.498Z"
-            fill="white"
-          />
-        </svg>
         <br />
       </Logo>
 
@@ -191,6 +167,24 @@ const HomePage = () => {
           </MenuBox>
         </Link>
 
+        <Link to={`/`}>
+          <MenuBox>
+            <img src={home_6} />
+            <span>
+              <span className="bold">나의 이야기</span> <br /> 기록하기
+            </span>
+          </MenuBox>
+        </Link>
+
+        <Link to={`/`}>
+          <MenuBox>
+            <img src={home_4} />
+            <span>
+              <span className="bold">메시지</span> <br /> 쓰러가기
+            </span>
+          </MenuBox>
+        </Link>
+
         <Link to={`/write`}>
           <MenuBox>
             <img src={home_3} />
@@ -202,9 +196,9 @@ const HomePage = () => {
 
         <Link to={`/`}>
           <MenuBox>
-            <img src={home_4} />
+            <img src={home_7} />
             <span>
-              <span className="bold">메시지</span> <br /> 쓰러가기
+              <span className="bold">Q&A</span> <br /> 나에 대하여
             </span>
           </MenuBox>
         </Link>
