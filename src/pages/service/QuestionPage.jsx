7c5f0c43f9ff58fcdc75030d8a6b9119d6d49123
minibxx <alsdbqls1004@gmail.com>
2024-03-23 01:52:45 +0900

    feat: 유서/ 메시지/ 질문 페이지 분리

---
+++ src/pages/service/QuestionPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Question from '@components/write/Question';
+
+function QuestionPage() {
+  return (
+    <>
+      <Question />
+    </>
+  );
+}
+
+export default QuestionPage;
