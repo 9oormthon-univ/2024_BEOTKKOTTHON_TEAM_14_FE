b239c2a43c026487bec239787cd082f7048ff9ea
sem-git <lsmsm0327@naver.com>
2024-03-23 20:09:00 +0900

    feat: 서버 주소 변경

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://3.37.117.95:8080/',
+        target: 'http://plandeath.store:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
